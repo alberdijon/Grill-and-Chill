e11da1406ba77bedb4c69c52449bf0229bd58865
Jon Telleria <telleria.jon@uni.eus>
2024-10-30 09:34:35 +0100

    feat(58)/order,delete-create-update
    order klasearen delete create update

--- GrillAndChill/Grill_And_Chill/forms.py
+++ GrillAndChill/Grill_And_Chill/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from .models import User, Product
+from .models import User, Product , Order
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'surname', 'password', 'gmail', 'tlf']
+        fields = ['name', 'surname', 'password', 'gmail', 'tlf', 'direction', 'apartment', 'Postal_code', 'city', 'state']
         
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'ingredients', 'price', 'category', 'foto']
+        
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['user_Id', 'price', 'ordered', 'direction', 'apartment', 'Postal_code', 'city', 'state']
--- GrillAndChill/Grill_And_Chill/views.py
+++ GrillAndChill/Grill_And_Chill/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404 , redirect
 from .models import User, Product, Order
-from .forms import UserForm , ProductForm
+from .forms import UserForm , ProductForm , OrderForm
 
 def index(request):
     return render(request, 'admintemplates/index.html')  # ensure the template path is correct
+
+# Basic Views for Users
 
 def user_list(request):
     users = User.objects.all()
@@ -12,25 +14,6 @@ def user_list(request):
 def user_detail(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     return render(request, 'admintemplates/user_detail.html', {'user': user})
-
-# Basic View for Products
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'admintemplates/product_list.html', {'products': products})
-
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'admintemplates/product_detail.html', {'product': product})
-
-# Basic View for Orders
-def order_list(request):
-    orders = Order.objects.all()
-    return render(request, 'admintemplates/order_list.html', {'orders': orders})
-
-def order_detail(request, order_id):
-    order = get_object_or_404(Order, pk=order_id)
-    return render(request, 'admintemplates/order_detail.html', {'order': order})
-
 
 def user_create(request):
     if request.method == 'POST':
@@ -42,12 +25,38 @@ def user_create(request):
         form = UserForm()
     return render(request, 'admintemplates/user_form.html', {'form': form})
 
+def user_update(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)  # Bind form to the user instance
+        if form.is_valid():
+            form.save()  # Save the updated user
+            return redirect('user_list')  # Redirect to the user list after successful update
+    else:
+        form = UserForm(instance=user)  # Populate the form with the user's current details
+    
+    return render(request, 'admintemplates/user_form.html', {'form': form, 'user': user})
+
 def user_delete(request, user_id):
     user = get_object_or_404(User, pk=user_id)
+    
     if request.method == 'POST':
-        user.delete()
-        return redirect('user_list')
+        user.delete()  # Delete the user
+        return redirect('user_list')  # Redirect to the user list after deletion
+    
     return render(request, 'admintemplates/user_confirm_delete.html', {'user': user})
+
+
+# Basic Views for Products
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'admintemplates/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'admintemplates/product_detail.html', {'product': product})
 
 def product_update(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
@@ -80,24 +89,55 @@ def product_create(request):
     
     return render(request, 'admintemplates/product_form.html', {'form': form})  # Render the form
 
-def user_update(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)  # Bind form to the user instance
-        if form.is_valid():
-            form.save()  # Save the updated user
-            return redirect('user_list')  # Redirect to the user list after successful update
-    else:
-        form = UserForm(instance=user)  # Populate the form with the user's current details
-    
-    return render(request, 'admintemplates/user_form.html', {'form': form, 'user': user})
+# Basic Views for Orders
 
-def user_delete(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
+# List View
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'admintemplates/order_list.html', {'orders': orders})
+
+# Detail View
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    return render(request, 'admintemplates/order_detail.html', {'order': order})
+
+# Create View
+def order_create(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new order
+            return redirect('order_list')  # Redirect to the order list
+    else:
+        form = OrderForm()  # Create an empty form if GET request
+
+    # Add Bootstrap class to all fields
+    for field in form.fields.values():
+        field.widget.attrs.update({'class': 'form-control'})
+
+    return render(request, 'admintemplates/order_form.html', {'form': form})
+
+
+# Update View
+def order_update(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
     
     if request.method == 'POST':
-        user.delete()  # Delete the user
-        return redirect('user_list')  # Redirect to the user list after deletion
+        form = OrderForm(request.POST, instance=order)  # Bind form to the order instance
+        if form.is_valid():
+            form.save()  # Save the updated order
+            return redirect('order_list')  # Redirect to the order list after successful update
+    else:
+        form = OrderForm(instance=order)  # Populate the form with the order's current details
     
-    return render(request, 'admintemplates/user_confirm_delete.html', {'user': user})
+    return render(request, 'admintemplates/order_form.html', {'form': form, 'order': order})
+
+# Delete View
+def order_delete(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    
+    if request.method == 'POST':
+        order.delete()  # Delete the order
+        return redirect('order_list')  # Redirect to the order list after deletion
+    
+    return render(request, 'admintemplates/order_confirm_delete.html', {'order': order})
