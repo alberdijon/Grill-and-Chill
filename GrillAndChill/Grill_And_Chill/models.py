067c58a77c359cfea29701799b2a20cabd629af7
JAlberdi11 <alberdi.jon@uni.eus>
2024-11-04 12:03:27 +0100

    fix (#27): modeloak aldatuta

--- GrillAndChill/Grill_And_Chill/models.py
+++ GrillAndChill/Grill_And_Chill/models.py
@@ -10,15 +10,17 @@ class User(models.Model):
     gmail = models.EmailField(max_length=100)
     tlf = models.IntegerField()
     direction = models.CharField(max_length=100)
-    apartment = models.CharField(max_length=100)
-    Postal_code = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-
 
     def __str__(self):
-        return f"{self.name} -- {self.surname} -- {self.password} -- {self.gmail} -- {self.tlf}"
+        return f"{self.name} -- {self.surname} -- {self.password} -- {self.gmail} -- {self.tlf} -- {self.direction}"
 
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id} -- {self.description}"
 
 class Alergen(models.Model):
     id = models.AutoField(primary_key=True)
@@ -34,10 +36,11 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     ingredients = models.CharField(max_length=1000)
     price = models.FloatField(max_length=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     foto = models.TextField()
 
     def __str__(self):
-        return f"{self.id} -- {self.name} -- {self.description} -- {self.ingredients} -- {self.alergens} -- {self.price} -- {self.foto}"
+        return f"{self.id} -- {self.name} -- {self.description} -- {self.ingredients} -- {self.price} -- {self.foto} "
     
 
 class Product_Alergen(models.Model):
@@ -49,20 +52,23 @@ class Product_Alergen(models.Model):
         return f"{self.code} -- {self.products_Id} -- {self.alergens_Id}"
 
 
-class Orders(models.Model):
+class Order(models.Model):
     id = models.AutoField(primary_key=True)
     user_Id = models.ForeignKey(User, on_delete=models.CASCADE)
-    products_Id  = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.FloatField()
-    order_Date = models.TimeField(auto_now=True)
+    order_Date = models.DateField(auto_now=True)
     ordered = models.FloatField()
     ended = models.BooleanField(default=False)
     direction = models.CharField(max_length=100)
-    apartment = models.CharField(max_length=100)
-    Postal_code = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.id} -- {self.user_Id} -- {self.products_Id} -- {self.price} -- {self.order_Date} -- {self.ordered}"
+        return f"{self.id} -- {self.user_Id} -- {self.price} -- {self.order_Date} -- {self.ordered} -- {self.direction}"
 
+
+class Product_Order(models.Model):
+    code = models.AutoField(primary_key=True)
+    products_Id  = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_Id  = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.code} -- {self.products_Id} -- {self.order_Id}"
