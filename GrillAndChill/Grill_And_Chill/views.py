1d330cf928006198eb04e75a62bd56f803b18e68
Jon Telleria <114485773+JonTelleria@users.noreply.github.com>
2024-11-08 09:22:18 +0100

    feature(#68)/soilik produktuak jaso (#89)
    restAPI laburtu dugu produktuak soilik jasotzeko

--- GrillAndChill/Grill_And_Chill/views.py
+++ GrillAndChill/Grill_And_Chill/views.py
@@ -243,6 +243,49 @@ def clientside_register(request):
   
   
 
+
+
+
+class ProductAPIView(APIView):
+    
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+#    def post(self, request, format=None):
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductAPIViewDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    # def put(self, request, pk, format=None):
+    #     product = self.get_object(pk)
+    #     serializer = ProductSerializer(product, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def delete(self, request, pk, format=None):
+    #     product = self.get_object(pk)
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+""" 
 class UserAPIView(APIView):
     
     def get(self, request, format=None):
@@ -367,50 +410,6 @@ class AlergenAPIViewDetail(APIView):
         alergen = self.get_object(pk)
         alergen.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class ProductAPIView(APIView):
-    
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProductAPIViewDetail(APIView):
-    
-    def get_object(self, pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 class ProductAlergenAPIView(APIView):
     
     def get(self, request, format=None):
@@ -536,3 +535,4 @@ class ProductOrderAPIViewDetail(APIView):
         product_order.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+ """
