23bbccccbad3185a6aa229a9a21157c1233b9c37
Jon Telleria <114485773+JonTelleria@users.noreply.github.com>
2024-11-11 08:57:02 +0100

    Feature/hash password (#96)
    * feat(#95)/hash password
    
    hasheo de pass
    
    * feat(#95)/hash password
    
    fix urls

--- GrillAndChill/Grill_And_Chill/urls.py
+++ GrillAndChill/Grill_And_Chill/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.index, name='index'),  
     path('users/', views.user_list, name='user_list'),
@@ -25,7 +25,7 @@ urlpatterns = [
     
     path('orders/revenue/', views.monthly_revenue, name='monthly_revenue'),
 
-    path('clientside/', views.clientside_main, name='clientside_main'),
+    path('clientside/', views.clientside_index, name='clientside_main'),
     path('clientside/register/', views.clientside_register, name='clientside_register'),
     path('clientside/log-in/', views.clientside_login, name='clientside_login'),
   
@@ -36,12 +36,12 @@ urlpatterns = [
 
   
   
-    path('v1/users/', views.UserAPIView.as_view()),
-    path('v1/users/<int:pk>/', views.UserAPIViewDetail.as_view()),
-    path('v1/categories/', views.CategoryAPIView.as_view()),
-    path('v1/categories/<int:pk>/', views.CategoryAPIViewDetail.as_view()),
-    path('v1/alergens/', views.AlergenAPIView.as_view()), 
-    path('v1/alergens/<int:pk>/', views.AlergenAPIViewDetail.as_view()),
+    # path('v1/users/', views.UserAPIView.as_view()),
+    # path('v1/users/<int:pk>/', views.UserAPIViewDetail.as_view()),
+    # path('v1/categories/', views.CategoryAPIView.as_view()),
+    # path('v1/categories/<int:pk>/', views.CategoryAPIViewDetail.as_view()),
+    # path('v1/alergens/', views.AlergenAPIView.as_view()), 
+    # path('v1/alergens/<int:pk>/', views.AlergenAPIViewDetail.as_view()),
   
 
   
@@ -49,6 +49,8 @@ urlpatterns = [
 
     path('v1/products/', views.ProductAPIView.as_view()),
     path('v1/products/<int:pk>/', views.ProductAPIViewDetail.as_view()),
+    
+   
     # path('v1/users/', views.UserAPIView.as_view()),
     # path('v1/users/<int:pk>/', views.UserAPIViewDetail.as_view()),
     # path('v1/categories/', views.CategoryAPIView.as_view()),
@@ -62,6 +64,6 @@ urlpatterns = [
     # path('v1/product_orders/', views.ProductOrderAPIView.as_view()),
     # path('v1/product_orders/<int:pk>/', views.ProductOrderAPIViewDetail.as_view()),
 
-
+    
 ]
    
