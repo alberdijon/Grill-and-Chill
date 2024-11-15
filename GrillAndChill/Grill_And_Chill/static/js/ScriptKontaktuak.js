65243df4fe6bee40e0fdedbaef494a7876878904
julengalin <114486026+julengalin@users.noreply.github.com>
2024-11-15 10:45:51 +0100

    Style.css (#104)

--- GrillAndChill/Grill_And_Chill/static/js/ScriptKontaktuak.js
+++ GrillAndChill/Grill_And_Chill/static/js/ScriptKontaktuak.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-    $('a.nav-link').hover(
-        function() {
-            $(this).animate({ fontSize: '1.5em' }, 500);
-        }, 
-        function() {
-            $(this).animate({ fontSize: '1em' }, 500);
-        }
-    );
+        
     const scrollText = document.querySelector(".scroll-text");
 
     function handleScroll() {
